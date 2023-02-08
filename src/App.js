99d83783dca737dbb32b16e9c272e99655286557
augustobor <augus_borromeo@hotmail.com>
2023-02-08 13:32:24 -0300

    card and router made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import Nav from "./componentes/nav/nav.jsx"
 
+import Home from "./contenedores/home.jsx"
+import NotFound from "./componentes/notFound/notFound.jsx"
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//Routas de la app
 function App() {
   return (
     <div className="App">
       <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/contenedores/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import TarjetaDestacada from '../componentes/tarjetaDestacada/tarjetaDestacada.jsx';
+const home = () => {
+  return (
+    <>
+      <TarjetaDestacada />
+    </>
+  )
+}
+
+export default home
