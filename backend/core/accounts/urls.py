2ccd756937775cedffdb3225bca547099f3feb88
Lizard <lizardwine@hotmail.com>
2023-02-09 12:16:32 -0600

    eliminando lineas inecessarias que causaban errores

--- backend/core/accounts/urls.py
+++ backend/core/accounts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
+
 ]
--- backend/core/accounts/views.py
+++ backend/core/accounts/views.py
@@ -3,6 +3,4 @@ from rest_framework.response import Response
 from django.http import FileResponse
 from rest_framework import permissions
 import rest_framework
-from . import utilities
-from . import models
 
