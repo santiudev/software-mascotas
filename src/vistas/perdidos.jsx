410c6d4655dce5e97a6479c3d0acc50f51101342
Letalandroid <carlossoncra@gmail.com>
2023-02-13 10:13:08 -0500

    Añadiendo Footer, styles y Tarjeta de Perdidos a src/vistas/perdidos

--- src/vistas/perdidos.jsx
+++ src/vistas/perdidos.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
-import Nav from "../componentes/nav/nav";
+import Nav from '../componentes/nav/nav';
+import Footer from '../../src/componentes/Footer/Footer';
+import styles from '../css/Perdidos/perdidos.module.css';
+import PerdidosTarjeta from '../componentes/Perdidos/perdidosTarjeta';
+import { Data } from '../Data_HO-PET';
+import Paginacion from '../componentes/Perdidos/paginacion';
 
 const Perdidos = () => {
-  return (
-    <>
-      <Nav />
-      <h1>Perdidos</h1>
-    </>
-  )
-}
+	return (
+		<>
+			<Nav />
+			<h2 className={styles.perdidos__h2}>
+				Mascotas Perdidas - Ayuda a encontrar a tu amigo peludo
+			</h2>
+			<p className={styles.perdidos__p}>
+				“Aquí podrás encontrar información sobre mascotas perdidas y colaborar
+				en su búsqueda. Si tienes información sobre alguna mascota perdida o has
+				encontrado una, por favor, comparte la información aquí para ayudar a
+				reunir a la mascota con su dueño.”
+			</p>
+			<section className={styles.perdidos__tarjeta_container}>
+				{Data.map(
+					({ id, tamaño, fecha, lugar, contacto, descripcion, imagen }) => {
+						return (
+							<PerdidosTarjeta
+								key={id}
+								url_img={imagen}
+								contacto={contacto}
+								desc={descripcion}
+								fecha={fecha}
+								nombre={id}
+								tam={tamaño}
+								zona={lugar}
+							/>
+						);
+					}
+				)}
+			</section>
+      <Paginacion />
+			<Footer />
+		</>
+	);
+};
 
-export default Perdidos
+export default Perdidos;
