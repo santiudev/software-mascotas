cb4ed48808cf317ac53020ba969cc2b76eb34aa3
Letalandroid <carlossoncra@gmail.com>
2023-02-14 11:25:14 -0500

    Colocando todo dentro de un contenedor para el bg

--- src/vistas/perdidos.jsx
+++ src/vistas/perdidos.jsx
@@ -12,42 +12,44 @@ const Perdidos = () => {
 	return (
 		<>
 			<Nav />
-			<section className={styles["texto-container"]}>
-				<article className={styles["texto-container__titulo"]}>
-					<LetraTitulo
-						texto="Mascotas perdidas - Ayuda a encontrar a tu amigo peludo"
-						clase="letra-titulo--red"
-					/>
-				</article>
-				<article className={styles["texto-container__subtitulo"]}>
-					<LetraParrafo
-						texto="“Aquí podrás encontrar información sobre mascotas perdidas y colaborar
+			<div className={styles.perdidos__container}>
+				<section className={styles['texto-container']}>
+					<article className={styles['texto-container__titulo']}>
+						<LetraTitulo
+							texto="Mascotas perdidas - Ayuda a encontrar a tu amigo peludo"
+							clase="letra-titulo--red"
+						/>
+					</article>
+					<article className={styles['texto-container__subtitulo']}>
+						<LetraParrafo
+							texto="“Aquí podrás encontrar información sobre mascotas perdidas y colaborar
 						en su búsqueda. Si tienes información sobre alguna mascota perdida o has
 						encontrado una, por favor, comparte la información aquí para ayudar a
 						reunir a la mascota con su dueño.”"
-						clase="letra-parrafo--black"
-					/>
-				</article>
-			</section>
-			<section className={styles.perdidos__tarjeta_container}>
-				{Data.map(
-					({ id, tamaño, fecha, lugar, contacto, descripcion, imagen }) => {
-						return (
-							<PerdidosTarjeta
-								key={id}
-								url_img={imagen}
-								contacto={contacto}
-								desc={descripcion}
-								fecha={fecha}
-								nombre={id}
-								tam={tamaño}
-								zona={lugar}
-							/>
-						);
-					}
-				)}
-			</section>
-      <Paginacion />
+							clase="letra-parrafo--black"
+						/>
+					</article>
+				</section>
+				<section className={styles.perdidos__tarjeta_container}>
+					{Data.map(
+						({ id, tamaño, fecha, lugar, contacto, descripcion, imagen }) => {
+							return (
+								<PerdidosTarjeta
+									key={id}
+									url_img={imagen}
+									contacto={contacto}
+									desc={descripcion}
+									fecha={fecha}
+									nombre={id}
+									tam={tamaño}
+									zona={lugar}
+								/>
+							);
+						}
+					)}
+				</section>
+				<Paginacion />
+			</div>
 			<Footer />
 		</>
 	);
