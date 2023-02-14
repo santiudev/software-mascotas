8cf43cfe3db373f9c02f7be70a1ba531e993af6f
Letalandroid <carlossoncra@gmail.com>
2023-02-14 11:18:43 -0500

    ''

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -4,12 +4,13 @@ import SectionOne from './SectionOne';
 import SectionTwo from './SectionTwo';
 import SectionThree from './SectionThree';
 import Copyright from './Copyright';
+import huellas from '../../iconos/imagen de fondo de huellas/bg-footer.png';
 
 export default function Footer() {
 	return (
 		<>
 			<footer className={styles.footer}>
-				<div className="huellas"></div>
+				<img className={styles.footer__huellas} src={huellas} alt='huellas' />
 				<SectionOne
 					title="HO-PET"
 					parrafo="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry"
@@ -27,7 +28,11 @@ export default function Footer() {
 				/>
 				<SectionTwo
 					title="Contacto"
-					enlaces={['+1 909 7640024', 'lorem@lorem.com', 'lorem@lorem.com']}
+					// contact={['+1 909 7640024', 'lorem@lorem.com', 'lorem@lorem.com']}
+					contact={{
+						tlf: ['+1 909 7640024'],
+						emails: ['lorem@lorem.com', 'lorem@lorem.com'],
+					}}
 				/>
 				<SectionThree title="Suscríbete al boletin" />
 			</footer>
