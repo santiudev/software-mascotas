bf00e184ef3586438d86f7caa4aed3106afd60a9
Letalandroid <carlossoncra@gmail.com>
2023-02-16 19:42:23 -0500

    Update adoptar.jsx
    Usando el Data_HO-PET de ejemplo para la AdoptarTarjeta

--- src/vistas/adoptar.jsx
+++ src/vistas/adoptar.jsx
@@ -6,12 +6,16 @@ import Nav from '../componentes/nav/nav';
 import Footer from '../componentes/Footer/Footer';
 import Paginacion from '../componentes/Perdidos/paginacion';
 import AdoptarTarjeta from '../componentes/adoptar/adoptarTarjeta';
+import Filtrar from '../componentes/adoptar/filtrar.module';
 import LetraTitulo from '../componentes/principales/LetraTitulo';
 import LetraParrafo from '../componentes/principales/letraParrafo';
 
 // Estilos
 import styles from '../css/adoptar/adoptar.module.css';
 import styles_perdidos from '../css/Perdidos/perdidos.module.css';
+
+// Test
+import { Data } from '../Data_HO-PET';
 
 const adoptar = () => {
 	return (
@@ -30,37 +34,20 @@ const adoptar = () => {
 						clase="letra-parrafo--black"
 					/>
 				</article>
+				<article className={styles.filter__container}>
+					<Filtrar />
+				</article>
 				<article className={styles.adoptarTarjeta__container}>
-					<AdoptarTarjeta
-						img="https://nfnatcane.es/blog/wp-content/uploads/2022/10/historia-de-la-raza-chihuahua.jpg"
-						descripcion="Encuentra a tu compañero fiel en nuestra lista de mascotas en adopción. Tenemos variedad de mascotas en busca de un hogar amoroso. Adoptar es una gran responsabilidad pero también una gran recompensa. ¡Haz feliz a una mascota adoptando hoy!"
-						contacto="999 999 999"
-					/>
-					<AdoptarTarjeta
-						img="https://nfnatcane.es/blog/wp-content/uploads/2022/10/historia-de-la-raza-chihuahua.jpg"
-						descripcion="Encuentra a tu compañero fiel en nuestra lista de mascotas en adopción. Tenemos variedad de mascotas en busca de un hogar amoroso. Adoptar es una gran responsabilidad pero también una gran recompensa. ¡Haz feliz a una mascota adoptando hoy!"
-						contacto="999 999 999"
-					/>
-					<AdoptarTarjeta
-						img="https://nfnatcane.es/blog/wp-content/uploads/2022/10/historia-de-la-raza-chihuahua.jpg"
-						descripcion="Encuentra a tu compañero fiel en nuestra lista de mascotas en adopción. Tenemos variedad de mascotas en busca de un hogar amoroso. Adoptar es una gran responsabilidad pero también una gran recompensa. ¡Haz feliz a una mascota adoptando hoy!"
-						contacto="999 999 999"
-					/>
-					<AdoptarTarjeta
-						img="https://nfnatcane.es/blog/wp-content/uploads/2022/10/historia-de-la-raza-chihuahua.jpg"
-						descripcion="Encuentra a tu compañero fiel en nuestra lista de mascotas en adopción. Tenemos variedad de mascotas en busca de un hogar amoroso. Adoptar es una gran responsabilidad pero también una gran recompensa. ¡Haz feliz a una mascota adoptando hoy!"
-						contacto="999 999 999"
-					/>
-					<AdoptarTarjeta
-						img="https://nfnatcane.es/blog/wp-content/uploads/2022/10/historia-de-la-raza-chihuahua.jpg"
-						descripcion="Encuentra a tu compañero fiel en nuestra lista de mascotas en adopción. Tenemos variedad de mascotas en busca de un hogar amoroso. Adoptar es una gran responsabilidad pero también una gran recompensa. ¡Haz feliz a una mascota adoptando hoy!"
-						contacto="999 999 999"
-					/>
-					<AdoptarTarjeta
-						img="https://nfnatcane.es/blog/wp-content/uploads/2022/10/historia-de-la-raza-chihuahua.jpg"
-						descripcion="Encuentra a tu compañero fiel en nuestra lista de mascotas en adopción. Tenemos variedad de mascotas en busca de un hogar amoroso. Adoptar es una gran responsabilidad pero también una gran recompensa. ¡Haz feliz a una mascota adoptando hoy!"
-						contacto="999 999 999"
-					/>
+					{Data.map((value) => {
+						return (
+							<AdoptarTarjeta
+								key={value.id}
+								img={value.imagen}
+								descripcion={value.descripcion}
+								contacto={value.contacto}
+							/>
+						);
+					})}
 				</article>
 				<Paginacion enlaces={['', '', '', '', '']} />
 			</section>
