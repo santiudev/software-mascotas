beaac0bc816f5ff7b9b2cd3f1644de992acc5aeb
augustobor <augus_borromeo@hotmail.com>
2023-02-28 14:23:15 -0300

    vista de refugio casi lista

--- src/componentes/refugios/RefugioCard.jsx
+++ src/componentes/refugios/RefugioCard.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 
 import styles from '../../css/refugios/refugiosCard.module.css'
 
-const RefugioCard = ({title, image}) => {
+const RefugioCard = ({title, image, clase}) => {
+
+  const setClass = (clase) => {
+    let customStyle = {}
+    switch(clase) {
+      case "square": 
+        customStyle = { gridRow: "span 4", gridColumn: "span 4"}
+        break;
+      case "small":
+        customStyle = { gridRow: "span 4", gridColumn: "span 3"}
+        break;
+      case "large":
+        customStyle = { gridRow: "span 4", gridColumn: "span 6"}
+        break;
+      default: customStyle = { gridRow: 1, gridColumn: 1}
+    }
+
+    return customStyle
+  }
+
   return (
-    <article className={styles.refugiosCard}>
+    <article className={styles.refugiosCard} style={setClass(clase)} >
       <img src={image} alt={title} />
     </article>
   )
