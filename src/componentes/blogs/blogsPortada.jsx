a78e6583c3786731a3b11b242530a4f2711e5428
Letalandroid <carlossoncra@gmail.com>
2023-03-01 16:52:35 -0500

    Blogs 80%
    Avanzando el blog hasta un 80%, solo faltaria el final que es la frase, fecha, redes y post relacionados

--- src/componentes/blogs/blogsPortada.jsx
+++ src/componentes/blogs/blogsPortada.jsx
@@ -1,14 +1,18 @@
 import styles from '../../css/blogs/blogsPortada.module.css';
 
-export default function BlogsPortada ({ img, titulo }) {
-  return (
+export default function BlogsPortada({ img, titulo, consejos }) {
+	return (
 		<div
 			className={styles.blogsPortada__container}
 			style={{ backgroundImage: `url(${img})` }}>
-			<div className={styles.blogsPortada__data}>
-				<span>Consejos</span>
-				<h2>{titulo}</h2>
-			</div>
+			{consejos ? (
+				<div className={styles.blogsPortada__data}>
+					<span>Consejos</span>
+					<h2>{titulo}</h2>
+				</div>
+			) : (
+				''
+			)}
 		</div>
 	);
 }
