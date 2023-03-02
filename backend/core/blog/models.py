0b7ae292bfbcc4ad7a89d5f0bfdeb57fd91d2c75
Santiago Urban <urbansanty@gmail.com>
2023-03-02 15:44:10 -0300

    Reducir peso imagen blog

--- backend/core/blog/models.py
+++ backend/core/blog/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from PIL import Image
+import io
+from django.core.files.base import ContentFile
+
+
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
@@ -10,3 +15,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if self.img:
+            # abre la imagen
+            img = Image.open(self.img)
+            # crea un buffer de memoria
+            buffer = io.BytesIO()
+            # guarda la imagen en el buffer con una calidad del 80%
+            img.save(buffer, format='JPEG', quality=80)
+            # guarda los datos de la imagen en una variable de bytes
+            image_data = buffer.getvalue()
+            # sobrescribe el archivo de imagen original con la imagen comprimida
+            self.img.save(self.img.name, ContentFile(image_data), save=False)
+        super().save(*args, **kwargs)
