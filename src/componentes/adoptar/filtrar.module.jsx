a7cfc40a7f8d5ff5694c762c53f3bedcdc73e864
Letalandroid <carlossoncra@gmail.com>
2023-03-10 14:32:15 -0500

    Salir desde la el boton

--- src/componentes/adoptar/filtrar.module.jsx
+++ src/componentes/adoptar/filtrar.module.jsx
@@ -15,10 +15,15 @@ const Filtrar = () => {
 				placeholder="-- Todos --"
 				id="input"
 			/>
-			<img className={styles.filtrar__down} src={downIcon} alt="down" />
+			<img
+				className={styles.filtrar__down}
+				src={downIcon}
+				alt="down"
+				style={verValores ? { transform: 'rotate(180deg)' } : {}}
+			/>
 			<div
 				className={styles.filter__option}
-				style={verValores ? { display: 'flex' } : {}}>
+				style={verValores ? { height: '140px' } : {}}>
 				<Opcion tipo="-- Todos --" view={setVerValores} />
 				<Opcion tipo="Perros" view={setVerValores} />
 				<Opcion tipo="Gatos" view={setVerValores} />
