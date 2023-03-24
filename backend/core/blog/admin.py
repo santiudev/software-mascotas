d7a3d4ab14b324da860f99a6d0435510741a9b38
Santiago Urban <urbansanty@gmail.com>
2023-03-24 15:49:42 -0300

    Se modifico api blog

--- backend/core/blog/admin.py
+++ backend/core/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post
 
-
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at')
+admin.site.register(Post, PostAdmin)
