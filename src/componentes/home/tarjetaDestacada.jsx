79bdc944c9a9aa94fb687bf5c2b8ffa179b658c4
Letalandroid <carlossoncra@gmail.com>
2023-03-27 18:15:30 -0500

    agregar nombres a la seccion de perdidos en el home

--- src/componentes/home/tarjetaDestacada.jsx
+++ src/componentes/home/tarjetaDestacada.jsx
@@ -14,35 +14,54 @@ import arrow from '../../iconos/arrow-down-sign-to-navigate.png'
 
 
 //tarjeta destacada recibe como prop la imagen de la mascota. Se require algún contenedor de tarjetas para eso
-const tarjetaDestacada = ({ image, length, date, place ,contact, description }) => {
-  const [stateHover, setStateHover] = useState(false)
-  const handleStateHover = () => {
-    setStateHover(true)
-  }
-  return(
-    <>
-      <article className={styles.card} onMouseOver={handleStateHover}>
-        <img src={image} alt="mascota" />
-        { stateHover && (
-          <section className={styles.cardExtended}>
-            <h3>Se perdió</h3>
-            <ul>
-              <li><b>Tamaño:</b> {length}</li>
-              <li><b>Zona:</b> {place}</li>
-              <li><b>Fecha:</b> {date}</li>
-              <li><b>Contacto:</b> {contact}</li>
-              <li><b>Descripción:</b> {description}</li>
-            </ul>
-            <address>
-            
-            </address>
-          </section>
-        )}
-        <button className={styles.cardButton}>
-          <img src={arrow} alt="arrow" />
-        </button>
-      </article>
-    </>
+const tarjetaDestacada = ({
+	nombre,
+  image,
+	length,
+	date,
+	place,
+	contact,
+	description,
+}) => {
+	const [stateHover, setStateHover] = useState(false);
+	const handleStateHover = () => {
+		setStateHover(true);
+	};
+	return (
+		<>
+			<article className={styles.card} onMouseOver={handleStateHover}>
+				<div className={styles.cardNoExtended}>
+					<img src={image} alt="mascota" />
+					<p>{nombre}</p>
+				</div>
+				{stateHover && (
+					<section className={styles.cardExtended}>
+						<h3>Se perdió</h3>
+						<ul>
+							<li>
+								<b>Tamaño:</b> {length}
+							</li>
+							<li>
+								<b>Zona:</b> {place}
+							</li>
+							<li>
+								<b>Fecha:</b> {date}
+							</li>
+							<li>
+								<b>Contacto:</b> {contact}
+							</li>
+							<li>
+								<b>Descripción:</b> {description}
+							</li>
+						</ul>
+						<address></address>
+					</section>
+				)}
+				<button className={styles.cardButton}>
+					<img src={arrow} alt="arrow" />
+				</button>
+			</article>
+		</>
 	);
 };
 
