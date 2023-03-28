2b0fb6012823e4f004ca4184d3e1adf2ca9b2258
Letalandroid <carlossoncra@gmail.com>
2023-03-27 20:14:35 -0500

    modificar el estilo de las card de la seccion de perdidos en el home

--- src/componentes/Perdidos/perdidosTarjeta.jsx
+++ src/componentes/Perdidos/perdidosTarjeta.jsx
@@ -1,28 +1,59 @@
-import SubData from "./subData";
-import shareIcon from '../../iconos/share-icon.png';
+import SubData from './subData';
 import styles from '../../css/Perdidos/perdidosTarjeta.module.css';
+import { FbIcon, ShareIcon, TwitterIcon, WsspIcon } from '../../iconos/svg/icons_svg';
 
-export default function PerdidosTarjeta({ url_img, nombre, tam, zona, fecha, contacto, desc, encontrado }) {
-  return (
+export default function PerdidosTarjeta({
+	url_img,
+	nombre,
+	tam,
+	zona,
+	fecha,
+	contacto,
+	desc,
+	encontrado,
+}) {
+	const data = [nombre, tam, zona, fecha, contacto, desc];
+	const data_subtitle = [
+		'Nombre',
+		'Tamaño',
+		'Zona',
+		'Fecha',
+		'Contacto',
+		'Descripcion',
+	];
+
+	return (
 		<div className={styles.perdidosTarjeta}>
 			<img
 				className={styles.perdidosTarjeta__img}
 				src={url_img}
 				alt={`${nombre}-img`}
 			/>
-			<h3 className={styles.perdidosTarjeta__h3}>{ encontrado ? 'Encontrado' : `Se perdió "${nombre}"` }</h3>
+			<h3
+				className={
+					encontrado
+						? styles.perdidosTarjeta__h3_orange
+						: styles.perdidosTarjeta__h3_red
+				}>
+				{encontrado ? 'Encontrado' : `Se perdió "${nombre}"`}
+			</h3>
 			<div className={styles.perdidosTarjeta__subdata_container}>
-        <SubData className={styles.perdidosTarjeta__subdata} subtitle="Tamaño" data={tam} />
-        <SubData className={styles.perdidosTarjeta__subdata} subtitle="Zona" data={zona} />
-        <SubData className={styles.perdidosTarjeta__subdata} subtitle="Fecha" data={fecha} />
-        <SubData className={styles.perdidosTarjeta__subdata} subtitle="Contacto" data={contacto} />
-        <SubData className={styles.perdidosTarjeta__subdata} subtitle="Descripción" data={desc} />
-      </div>
-			<img
-				className={styles.perdidosTarjeta__img}
-				src={shareIcon}
-				alt="share-icon"
-			/>
+				{data.map((value, index) => {
+					return (
+						<SubData
+							className={styles.perdidosTarjeta__subdata}
+							subtitle={data_subtitle[index]}
+							data={value}
+						/>
+					);
+				})}
+			</div>
+			<div className={styles.perdidosTarjeta__redes}>
+				<WsspIcon encontrado={encontrado} />
+				<TwitterIcon encontrado={encontrado} />
+				<FbIcon encontrado={encontrado} />
+				<ShareIcon encontrado={encontrado} />
+			</div>
 		</div>
 	);
 }
