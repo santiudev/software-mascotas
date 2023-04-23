0d900906b0c802089d4ea4b3eb89f0ec228878dd
Leonel <lpaezbusiness@gmail.com>
2023-04-23 16:55:09 -0300

    Click afuera y scroll cierran el menu hamburguesa

--- src/componentes/nav/BurgerMenu/BurgerMenu.jsx
+++ src/componentes/nav/BurgerMenu/BurgerMenu.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import styles from "../../../css/nav/BurgerMenu/BurgerMenu.module.css"
 import BtnPrincipal from "../../principales/btnPrincipal";
 import Formulario from '../../Formulario/formulario';
@@ -9,6 +9,27 @@ import BurgerItem from "./BurgerItem";
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false)
     const [modal, setModal] = useState(false)
+    const divRef = useRef(null);
+
+    useEffect(() => {
+      const handleClickOutside = (event) => {
+        if (!divRef.current.contains(event.target)) {
+          setIsOpen(false);
+        }
+      };
+  
+      const handleScroll = () => {
+        setIsOpen(false);
+      };
+  
+      document.addEventListener('click', handleClickOutside);
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        document.removeEventListener('click', handleClickOutside);
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, [setIsOpen]);
 
     return(
         <div className={styles.burger_menu}>
@@ -29,7 +50,11 @@ const Navbar = () => {
               </li>
             </ul>
             {modal && <Formulario setModal={setModal} />}
-            <div className={`${styles.burger_menu__toggle} ${isOpen && styles.open}`} onClick={ () => setIsOpen(!isOpen)} >
+            <div
+              className={`${styles.burger_menu__toggle} ${isOpen && styles.open}`}
+              onClick={ () => setIsOpen(!isOpen)} 
+              ref={divRef}
+            >
                 <span></span>
                 <span></span>
                 <span></span>
