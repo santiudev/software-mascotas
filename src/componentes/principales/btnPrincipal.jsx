1981aaedbce881e90f7cbef37e0ba9015c41b78c
Letalandroid <carlossoncra@gmail.com>
2023-04-25 21:18:20 -0500

    Inputs requeridos en Contacto

--- src/componentes/principales/btnPrincipal.jsx
+++ src/componentes/principales/btnPrincipal.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import styles from "../../css/principales/btnPrincipal.module.css";
-import imgPaw from "../../iconos/paw.png";
+import React from 'react';
+import styles from '../../css/principales/btnPrincipal.module.css';
+import imgPaw from '../../iconos/paw.png';
 
-const BtnPrincipal = ({ texto, setModal }) => {
-	
+const BtnPrincipal = ({ texto, setModal, action }) => {
 	const handleSetModal = () => {
-		if (texto==="Reportar") {
-			setModal(true)
+		if (texto === 'Reportar') {
+			setModal(true);
 		}
-	}
+	};
 
-	return(
-		<button className={styles.btn} type="button" onClick={handleSetModal}>
-				<p className={styles.btn__text}>{texto}</p>
-				<img 
-					src={imgPaw}
-					className={styles.btn__img}
-					alt="buscador"
-				/>
+	return texto === 'Enviar' ? (
+		<button className={styles.btn}>
+			<input type="submit" className={styles.btn__text} value={texto} />
+			<img src={imgPaw} className={styles.btn__img} alt="buscador" />
+		</button>
+	) : (
+		<button className={styles.btn} type="button" onClick={() => handleSetModal}>
+			<p className={styles.btn__text}>{texto}</p>
+			<img src={imgPaw} className={styles.btn__img} alt="buscador" />
 		</button>
 	);
 };
